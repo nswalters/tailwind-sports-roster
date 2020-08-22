0b4f7f9ea0bcdac6ba1db9bdd7248e6df14d3d7a
Nick Walters <nswalters.s@gmail.com>
2020-08-22 08:34:53 -0500

    Added team logo to page;Fixed player name and position layout on large screens

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -11,10 +11,10 @@ class Player extends React.Component {
     const { player } = this.props;
 
     return (
-      <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+      <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10">
          <div className="space-y-6 xl:space-y-10">
            <img className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src={player.imageUrl} alt="" />
-           <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+           <div className="space-y-2">
              <div className="font-medium text-lg leading-6 space-y-1">
                <h4 className="text-white">{player.name}</h4>
                <p className="text-indigo-400">{player.position}</p>
